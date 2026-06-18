a6ea49d0194b4bc6ccc2a147585c33d443331c05
xnatthan <brunonata09@gmail.com>
2026-06-18 00:13:35 -0300

    feat: adiciona tela responsiva de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)/index" />
       <Stack.Screen name="(tabs)/explore" />
       <Stack.Screen name="modal" />
+      <Stack.Screen name="cadastro" />
     </Stack>
   );
 }
